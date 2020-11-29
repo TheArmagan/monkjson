4cdc7e29c9afa6d09c9fb2aea7fdc3e254658d86
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-11-29 13:19:30 +0300

    yes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let _ = {
     set: require("lodash/set"),
     get: require("lodash/get"),
     has: require("lodash/has"),
-    unset: require("lodash/unset")
+    unset: require("lodash/unset"),
+    update: require("lodash/update")
 }
 
 /** @type {Monk.IMonkManager} */
@@ -18,12 +19,19 @@ let isConnectionClosed = true;
  * @param {String} uri Connection uri
  * @param {String} collectionName The Monkjson's collection name. (Default: monkjson)
  */
-function setConnection(uri,collectionName="monkjson") {
+function setConnection(uri, collectionName = "monkjson") {
     isConnectionClosed = false;
     db = Monk.default(uri);
     mjCollection = db.get(collectionName);
     mjCollection.createIndex("name");
-    return {db, mjCollection};
+    return { db, mjCollection };
+}
+
+/** Can be undefined
+ * @returns {db: Monk.IMonkManager, mjCollection: Monk.ICollection}
+ *  */
+function getConnection() {
+    return { db, mjCollection }
 }
 
 /** 
@@ -45,36 +53,36 @@ function MonkJson(name) {
     if (!mjCollection || isConnectionClosed) throw "ERRCINS: Connection is not set.";
     if (typeof name != "string" && name) throw "ERRDSBS: Database name should be string.";
     this.name = name;
-    
+
     async function getDBJson() {
-        let mj = await mjCollection.findOne({name});
+        let mj = await mjCollection.findOne({ name });
         if (!mj) {
-            await mjCollection.insert({name, json: {}});
-            mj = await mjCollection.findOne({name});
+            await mjCollection.insert({ name, json: {} });
+            mj = await mjCollection.findOne({ name });
         }
         return mj.json;
     }
 
     async function setDBJson(dataToSet) {
-        let mj = await mjCollection.findOne({name});
+        let mj = await mjCollection.findOne({ name });
         if (!mj) {
-            await mjCollection.insert({name, json: {}});
-            mj = await mjCollection.findOne({name});
+            await mjCollection.insert({ name, json: {} });
+            mj = await mjCollection.findOne({ name });
         }
         mj.json = dataToSet;
-        await mjCollection.findOneAndUpdate({name},{$set:mj});
+        await mjCollection.findOneAndUpdate({ name }, { $set: mj });
         return dataToSet;
     }
 
-    this.system = {getDBJson, setDBJson};
+    this.system = { getDBJson, setDBJson, mjCollection, db };
 
     /**
      * @param {String} dataPath 
      * @param {*} dataValue
      */
-    this.set = this.put = this.insert = async (dataPath, dataValue)=>{
+    this.set = this.put = this.insert = async (dataPath, dataValue) => {
         let dbJson = await getDBJson();
-        let newJson = _.set(dbJson,dataPath,dataValue);
+        let newJson = _.set(dbJson, dataPath, dataValue);
         await setDBJson(newJson);
         return dataValue;
     }
@@ -82,7 +90,7 @@ function MonkJson(name) {
     /**
      * @param {String} dataPath
      */
-    this.get = async (dataPath)=>{
+    this.get = async (dataPath) => {
         let dbJson = await getDBJson();
         let resultData = _.get(dbJson, dataPath);
         return resultData;
@@ -91,7 +99,7 @@ function MonkJson(name) {
     /**
      * @param {String} dataPath
      */
-    this.has = this.exists = async (dataPath)=>{
+    this.has = this.exists = async (dataPath) => {
         let dbJson = await getDBJson();
         let resultData = _.has(dbJson, dataPath);
         return resultData;
@@ -102,7 +110,7 @@ function MonkJson(name) {
      */
     this.del = this.unset = async (dataPath) => {
         let dbJson = await getDBJson();
-        let isOk = _.unset(dbJson,dataPath); // this method mutates the original object
+        let isOk = _.unset(dbJson, dataPath); // this method mutates the original object
         await setDBJson(dbJson);
         return isOk;
     }
@@ -126,14 +134,14 @@ function MonkJson(name) {
         }
     }
 
-     /**
-     * @param {String} dataPath
-     * @param {*} value
-     */
+    /**
+    * @param {String} dataPath
+    * @param {*} value
+    */
     this.add = async (dataPath, value) => {
         let dbJson = await getDBJson();
         let oldVal = _.get(dbJson, dataPath);
-        let newVal = oldVal+value;
+        let newVal = oldVal + value;
         let resultData = _.set(dbJson, dataPath, newVal);
         await setDBJson(resultData);
         return newVal;
@@ -146,9 +154,20 @@ function MonkJson(name) {
     this.subtract = async (dataPath, value) => {
         let dbJson = await getDBJson();
         let oldVal = _.get(dbJson, dataPath);
-        let newVal = oldVal-value;
+        let newVal = oldVal - value;
         let resultData = _.set(dbJson, dataPath, newVal);
         await setDBJson(resultData);
+        return newVal;
+    }
+
+    /**
+     * @param {String} dataPath 
+     * @param {(value: any) => any} updater 
+     */
+    this.update = async (dataPath, updater) => {
+        let dbJson = await getDBJson();
+        let newVal = _.update(dbJson, dataPath, updater);
+        await setDBJson(dbJson);
         return newVal;
     }
 }
@@ -156,5 +175,6 @@ function MonkJson(name) {
 module.exports = {
     MonkJson,
     setConnection,
-    endConnection
+    endConnection,
+    getConnection
 }
--- readme.md
+++ readme.md
@@ -1,15 +1,42 @@
-# Quick Example
-```js
-let MJ = require("monkjson");
+> # 💨 Quick Example
+>
+> ```js
+> let MJ = require("monkjson");
+>
+> MJ.setConnection("mongodb://127.0.0.1/monkjson");
+>
+> let db = new MJ.MonkJson("db");
+>
+> (async () => {
+>   await db.set("test.hey.baby", Math.random());
+>   let d = await db.get("test");
+>   console.log(d); //-> { hey: { baby: 0.9972221858216976 } }
+>   MJ.endConnection();
+> })();
+> ```
 
-MJ.setConnection("mongodb://127.0.0.1/monkjson");
+> ## API:
+>
+> `dataPath` is always needs be string type.
+>
+> - ### `new MonkJon(name: string)` -
+>
+>   - `.name: String` - Name of the database.
+>   - `.set(dataPath, data): any | .put(dataPath, data): any`
+>   - `.get(dataPath): any`
+>   - `.has(dataPath): boolean | .exists(dataPath): boolean`
+>   - `.del(dataPath): boolean | .unset(dataPath): boolean` - It returns a boolean whether the operation was successful or not.
+>   - `.push(dataPath, ...values)`
+>   - `.add(dataPath, data)`
+>   - `.subtract(dataPath, data)`
+>   - `.update(dataPath, updater: function(value: any): any)` - Same as lodash's update function.
+>
+> - ### `setConnection(mongodbURI: string)`
+> - ### `endConnection()`
+> - ### `getConnection()`
 
-let db = new MJ.MonkJson("db");
-
-(async ()=>{
-    await db.set("test.hey.baby",Math.random());
-    let d = await db.get("test");
-    console.log(d); //-> { hey: { baby: 0.9972221858216976 } }
-    MJ.endConnection();
-})();
-```
+> ## 😎 Last update:
+>
+> - db.update added.
+> - getConnection added.
+> - Better documentation.
